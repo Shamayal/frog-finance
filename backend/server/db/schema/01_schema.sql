304cebdae2640b69849f00321a5cf00cc30ec54b
Shamayal Syed <shamayalsyed9@gmail.com>
2023-06-10 18:59:23 -0400

    database configuration, fixed errors

--- backend/server/db/schema/01_schema.sql
+++ backend/server/db/schema/01_schema.sql
@@ -1,14 +1,12 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS categories CASCADE;
-DROP TABLE IF EXISTS sub_categories CASCADE
+DROP TABLE IF EXISTS sub_categories CASCADE;
 DROP TABLE IF EXISTS income CASCADE;
-DROP TABLE IF EXISTS expenses CASCADE;
 DROP TABLE IF EXISTS budgets CASCADE;
+DROP TABLE IF EXISTS expenses CASCADE;
 DROP TABLE IF EXISTS savings CASCADE;
 DROP TABLE IF EXISTS debt_payments CASCADE;
 DROP TABLE IF EXISTS debt_goals CASCADE;
-
-
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -25,35 +23,37 @@ CREATE TABLE categories (
 
 CREATE TABLE sub_categories (
   id SERIAL PRIMARY KEY NOT NULL,
-  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   sub_category VARCHAR(255) NOT NULL
 );
 
 
 CREATE TABLE income (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER(255) REFERENCES users(id) ON DELETE CASCADE
-  income_date DATE DEFAULT NOT NULL,
-  amount INTEGER NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  income_date DATE NOT NULL,
+  amount INTEGER NOT NULL
 );
+
 
 CREATE TABLE budgets (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER(255) REFERENCES users(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   budget_amount INTEGER NOT NULL, 
-  category_id INTEGER(255) REFERENCES categories(id) ON DELETE CASCADE,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   total_spent INTEGER NOT NULL DEFAULT 0,
-  updated_at DATE DEFAULT NOT NULL,
+  updated_at DATE NOT NULL,
   budget_reached BOOLEAN NOT NULL DEFAULT FALSE
 );
 
+
 CREATE TABLE expenses (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER(255) REFERENCES users(id) ON DELETE CASCADE
-  expense_date DATE DEFAULT NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  expense_date DATE NOT NULL,
   amount INTEGER NOT NULL,
-  budget_id INTEGER(255) REFERENCES budgets(id) ON DELETE CASCADE
-  sub_category_id INTEGER(255) REFERENCES sub_categories(id) ON DELETE CASCADE
+  budget_id INTEGER REFERENCES budgets(id) ON DELETE CASCADE,
+  sub_category_id INTEGER REFERENCES sub_categories(id) ON DELETE CASCADE
 );
 
 CREATE TABLE savings (
@@ -61,10 +61,10 @@ CREATE TABLE savings (
   saving_name VARCHAR(255) NOT NULL,
   goal_amount INTEGER NOT NULL, 
   current_amount INTEGER NOT NULL, 
-  finished BOOLEAN NOT NULL DEFAULT FALSE
-  date_created DATE DEFAULT NOT NULL,
-  date_finished DATE DEFAULT NULL,
-  user_id INTEGER(255) REFERENCES users(id) ON DELETE CASCADE
+  finished BOOLEAN NOT NULL DEFAULT FALSE,
+  date_created DATE NOT NULL,
+  date_finished DATE NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE debt_goals (
@@ -74,13 +74,13 @@ CREATE TABLE debt_goals (
   amount_left INTEGER NOT NULL,
   interest_rate DECIMAL NOT NULL,
   paid_off BOOLEAN DEFAULT FALSE,
-  user_id INTEGER(255) REFERENCES users(id) ON DELETE CASCADE
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE debt_payments (
   id SERIAL PRIMARY KEY NOT NULL,
   amount INTEGER NOT NULL, 
-  debt_goal_id INTEGER(255) REFERENCES debt_goals(id) ON DELETE CASCADE,
-  user_id INTEGER(255) REFERENCES users(id) ON DELETE CASCADE
+  debt_goal_id INTEGER REFERENCES debt_goals(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
--- backend/server/db/seeds/01_seeds.sql
+++ backend/server/db/seeds/01_seeds.sql
@@ -1,106 +1,106 @@
 -- users
-INSERT INTO users (nickname, email) VALUES ("test user", "test@email.com");
+INSERT INTO users (nickname, email) VALUES ('user', 'test@email.com');
 
 -- categories
-INSERT INTO categories (category) VALUES ("Housing"); -- 1
-INSERT INTO categories (category) VALUES ("Food"); -- 2
-INSERT INTO categories (category) VALUES ("Transportation"); -- 3
-INSERT INTO categories (category) VALUES ("Healthcare"); -- 4
-INSERT INTO categories (category) VALUES ("Education"); -- 5
-INSERT INTO categories (category) VALUES ("Shopping"); -- 6
-INSERT INTO categories (category) VALUES ("Subscriptions"); -- 7
-INSERT INTO categories (category) VALUES ("Communication"); -- 8
-INSERT INTO categories (category) VALUES ("Entertainment"); -- 9
-INSERT INTO categories (category) VALUES ("Hobbies"); -- 10
-INSERT INTO categories (category) VALUES ("Travel"); -- 11
-INSERT INTO categories (category) VALUES ("Pets"); -- 12
-INSERT INTO categories (category) VALUES ("Other"); -- 13
+INSERT INTO categories (category) VALUES ('Housing'); -- 1
+INSERT INTO categories (category) VALUES ('Food'); -- 2
+INSERT INTO categories (category) VALUES ('Transportation'); -- 3
+INSERT INTO categories (category) VALUES ('Healthcare'); -- 4
+INSERT INTO categories (category) VALUES ('Education'); -- 5
+INSERT INTO categories (category) VALUES ('Shopping'); -- 6
+INSERT INTO categories (category) VALUES ('Subscriptions'); -- 7
+INSERT INTO categories (category) VALUES ('Communication'); -- 8
+INSERT INTO categories (category) VALUES ('Entertainment'); -- 9
+INSERT INTO categories (category) VALUES ('Hobbies'); -- 10
+INSERT INTO categories (category) VALUES ('Travel'); -- 11
+INSERT INTO categories (category) VALUES ('Pets'); -- 12
+INSERT INTO categories (category) VALUES ('Other'); -- 13
 
 -- sub_categories
 
 -- Housing = 1
-INSERT INTO sub_categories (category_id, sub_category) VALUES (1, "Utilities"); -- 1
-INSERT INTO sub_categories (category_id, sub_category) VALUES (1, "Rent/Mortgage"); -- 2
-INSERT INTO sub_categories (category_id, sub_category) VALUES (1, "Insurance"); -- 3
-INSERT INTO sub_categories (category_id, sub_category) VALUES (1, "Renovations and Maintenance"); -- 4
-INSERT INTO sub_categories (category_id, sub_category) VALUES (1, "Household Products"); -- 5
-INSERT INTO sub_categories (category_id, sub_category) VALUES (1, "Other"); -- 6 
+INSERT INTO sub_categories (category_id, sub_category) VALUES (1, 'Utilities'); -- 1
+INSERT INTO sub_categories (category_id, sub_category) VALUES (1, 'Rent/Mortgage'); -- 2
+INSERT INTO sub_categories (category_id, sub_category) VALUES (1, 'Insurance'); -- 3
+INSERT INTO sub_categories (category_id, sub_category) VALUES (1, 'Renovations and Maintenance'); -- 4
+INSERT INTO sub_categories (category_id, sub_category) VALUES (1, 'Household Products'); -- 5
+INSERT INTO sub_categories (category_id, sub_category) VALUES (1, 'Other'); -- 6 
 
 -- Food = 2
-INSERT INTO sub_categories (category_id, sub_category) VALUES (2, "Groceries"); -- 7
-INSERT INTO sub_categories (category_id, sub_category) VALUES (2, "Dining Out"); -- 8
-INSERT INTO sub_categories (category_id, sub_category) VALUES (2, "Beverages"); -- 9
-INSERT INTO sub_categories (category_id, sub_category) VALUES (2, "Other"); -- 10
+INSERT INTO sub_categories (category_id, sub_category) VALUES (2, 'Groceries'); -- 7
+INSERT INTO sub_categories (category_id, sub_category) VALUES (2, 'Dining Out'); -- 8
+INSERT INTO sub_categories (category_id, sub_category) VALUES (2, 'Beverages'); -- 9
+INSERT INTO sub_categories (category_id, sub_category) VALUES (2, 'Other'); -- 10
 
 -- Transportation = 3
-INSERT INTO sub_categories (category_id, sub_category) VALUES (3, "Gas"); -- 11
-INSERT INTO sub_categories (category_id, sub_category) VALUES (3, "Car insurance"); -- 12
-INSERT INTO sub_categories (category_id, sub_category) VALUES (3, "Public Transit"); -- 13
-INSERT INTO sub_categories (category_id, sub_category) VALUES (3, "Maintenance"); -- 14
-INSERT INTO sub_categories (category_id, sub_category) VALUES (3, "Other"); -- 15
+INSERT INTO sub_categories (category_id, sub_category) VALUES (3, 'Gas'); -- 11
+INSERT INTO sub_categories (category_id, sub_category) VALUES (3, 'Car insurance'); -- 12
+INSERT INTO sub_categories (category_id, sub_category) VALUES (3, 'Public Transit'); -- 13
+INSERT INTO sub_categories (category_id, sub_category) VALUES (3, 'Maintenance'); -- 14
+INSERT INTO sub_categories (category_id, sub_category) VALUES (3, 'Other'); -- 15
 
 -- Healthcare = 4
-INSERT INTO sub_categories (category_id, sub_category) VALUES (4, "Prescriptions"); -- 16
-INSERT INTO sub_categories (category_id, sub_category) VALUES (4, "Vision Care"); -- 17
-INSERT INTO sub_categories (category_id, sub_category) VALUES (4, "Dental Care"); -- 18
-INSERT INTO sub_categories (category_id, sub_category) VALUES (4, "Other"); -- 19
+INSERT INTO sub_categories (category_id, sub_category) VALUES (4, 'Prescriptions'); -- 16
+INSERT INTO sub_categories (category_id, sub_category) VALUES (4, 'Vision Care'); -- 17
+INSERT INTO sub_categories (category_id, sub_category) VALUES (4, 'Dental Care'); -- 18
+INSERT INTO sub_categories (category_id, sub_category) VALUES (4, 'Other'); -- 19
 
 -- Education = 5
-INSERT INTO sub_categories (category_id, sub_category) VALUES (5, "Tuition"); -- 20
-INSERT INTO sub_categories (category_id, sub_category) VALUES (5, "Books"); -- 21
-INSERT INTO sub_categories (category_id, sub_category) VALUES (5, "Supplies"); -- 22 
-INSERT INTO sub_categories (category_id, sub_category) VALUES (5, "Other"); -- 23
+INSERT INTO sub_categories (category_id, sub_category) VALUES (5, 'Tuition'); -- 20
+INSERT INTO sub_categories (category_id, sub_category) VALUES (5, 'Books'); -- 21
+INSERT INTO sub_categories (category_id, sub_category) VALUES (5, 'Supplies'); -- 22 
+INSERT INTO sub_categories (category_id, sub_category) VALUES (5, 'Other'); -- 23
 
 -- Shopping = 6
-INSERT INTO sub_categories (category_id, sub_category) VALUES (6, "Shoes and Clothes"); -- 24
-INSERT INTO sub_categories (category_id, sub_category) VALUES (6, "Electronics"); -- 25
-INSERT INTO sub_categories (category_id, sub_category) VALUES (6, "Makeup and Skin Care"); -- 26 
-INSERT INTO sub_categories (category_id, sub_category) VALUES (6, "Hygiene Products"); -- 27
-INSERT INTO sub_categories (category_id, sub_category) VALUES (6, "Other"); -- 28
+INSERT INTO sub_categories (category_id, sub_category) VALUES (6, 'Shoes and Clothes'); -- 24
+INSERT INTO sub_categories (category_id, sub_category) VALUES (6, 'Electronics'); -- 25
+INSERT INTO sub_categories (category_id, sub_category) VALUES (6, 'Makeup and Skin Care'); -- 26 
+INSERT INTO sub_categories (category_id, sub_category) VALUES (6, 'Hygiene Products'); -- 27
+INSERT INTO sub_categories (category_id, sub_category) VALUES (6, 'Other'); -- 28
 
 -- Subscriptions = 7
-INSERT INTO sub_categories (category_id, sub_category) VALUES (7, "Gym"); -- 29
-INSERT INTO sub_categories (category_id, sub_category) VALUES (7, "Streaming Services"); -- 30
-INSERT INTO sub_categories (category_id, sub_category) VALUES (7, "Gaming"); -- 31
-INSERT INTO sub_categories (category_id, sub_category) VALUES (7, "Memberships"); -- 32
-INSERT INTO sub_categories (category_id, sub_category) VALUES (7, "Other"); -- 33
+INSERT INTO sub_categories (category_id, sub_category) VALUES (7, 'Gym'); -- 29
+INSERT INTO sub_categories (category_id, sub_category) VALUES (7, 'Streaming Services'); -- 30
+INSERT INTO sub_categories (category_id, sub_category) VALUES (7, 'Gaming'); -- 31
+INSERT INTO sub_categories (category_id, sub_category) VALUES (7, 'Memberships'); -- 32
+INSERT INTO sub_categories (category_id, sub_category) VALUES (7, 'Other'); -- 33
 
 -- Communication = 8
-INSERT INTO sub_categories (category_id, sub_category) VALUES (8, "Phone Bill"); -- 34
-INSERT INTO sub_categories (category_id, sub_category) VALUES (8, "Internet"); -- 35
-INSERT INTO sub_categories (category_id, sub_category) VALUES (8, "Other"); -- 36
+INSERT INTO sub_categories (category_id, sub_category) VALUES (8, 'Phone Bill'); -- 34
+INSERT INTO sub_categories (category_id, sub_category) VALUES (8, 'Internet'); -- 35
+INSERT INTO sub_categories (category_id, sub_category) VALUES (8, 'Other'); -- 36
 
 -- Entertainment = 9
-INSERT INTO sub_categories (category_id, sub_category) VALUES (9, "Movies"); -- 37
-INSERT INTO sub_categories (category_id, sub_category) VALUES (9, "Concerts"); -- 38
-INSERT INTO sub_categories (category_id, sub_category) VALUES (9, "Events"); -- 39
-INSERT INTO sub_categories (category_id, sub_category) VALUES (9, "Hobbies"); -- 40
-INSERT INTO sub_categories (category_id, sub_category) VALUES (9, "Other"); -- 41
+INSERT INTO sub_categories (category_id, sub_category) VALUES (9, 'Movies'); -- 37
+INSERT INTO sub_categories (category_id, sub_category) VALUES (9, 'Concerts'); -- 38
+INSERT INTO sub_categories (category_id, sub_category) VALUES (9, 'Events'); -- 39
+INSERT INTO sub_categories (category_id, sub_category) VALUES (9, 'Hobbies'); -- 40
+INSERT INTO sub_categories (category_id, sub_category) VALUES (9, 'Other'); -- 41
 
 -- Hobbies = 10
-INSERT INTO sub_categories (category_id, sub_category) VALUES (10, "Equipment and Supplies"); -- 42
-INSERT INTO sub_categories (category_id, sub_category) VALUES (10, "Lessons or Coaching"); -- 43
-INSERT INTO sub_categories (category_id, sub_category) VALUES (10, "Events and Tickets"); -- 44
-INSERT INTO sub_categories (category_id, sub_category) VALUES (10, "Uniforms or Apparel"); -- 45
-INSERT INTO sub_categories (category_id, sub_category) VALUES (10, "Other"); -- 46
+INSERT INTO sub_categories (category_id, sub_category) VALUES (10, 'Equipment and Supplies'); -- 42
+INSERT INTO sub_categories (category_id, sub_category) VALUES (10, 'Lessons or Coaching'); -- 43
+INSERT INTO sub_categories (category_id, sub_category) VALUES (10, 'Events and Tickets'); -- 44
+INSERT INTO sub_categories (category_id, sub_category) VALUES (10, 'Uniforms or Apparel'); -- 45
+INSERT INTO sub_categories (category_id, sub_category) VALUES (10, 'Other'); -- 46
 
 -- Travel = 11
-INSERT INTO sub_categories (category_id, sub_category) VALUES (11, "Accomodations"); -- 47
-INSERT INTO sub_categories (category_id, sub_category) VALUES (11, "Transportation"); -- 48
-INSERT INTO sub_categories (category_id, sub_category) VALUES (11, "Food and Dining"); -- 49
-INSERT INTO sub_categories (category_id, sub_category) VALUES (11, "Activities"); -- 50
-INSERT INTO sub_categories (category_id, sub_category) VALUES (11, "Other"); -- 51
+INSERT INTO sub_categories (category_id, sub_category) VALUES (11, 'Accomodations'); -- 47
+INSERT INTO sub_categories (category_id, sub_category) VALUES (11, 'Transportation'); -- 48
+INSERT INTO sub_categories (category_id, sub_category) VALUES (11, 'Food and Dining'); -- 49
+INSERT INTO sub_categories (category_id, sub_category) VALUES (11, 'Activities'); -- 50
+INSERT INTO sub_categories (category_id, sub_category) VALUES (11, 'Other'); -- 51
 
 --Pets = 12
-INSERT INTO sub_categories (category_id, sub_category) VALUES (12, "Food"); -- 52
-INSERT INTO sub_categories (category_id, sub_category) VALUES (12, "Grooming"); -- 53 
-INSERT INTO sub_categories (category_id, sub_category) VALUES (12, "Accessories"); -- 54 
-INSERT INTO sub_categories (category_id, sub_category) VALUES (12, "Veterinary"); -- 55
-INSERT INTO sub_categories (category_id, sub_category) VALUES (12, "Other"); -- 56
+INSERT INTO sub_categories (category_id, sub_category) VALUES (12, 'Food'); -- 52
+INSERT INTO sub_categories (category_id, sub_category) VALUES (12, 'Grooming'); -- 53 
+INSERT INTO sub_categories (category_id, sub_category) VALUES (12, 'Accessories'); -- 54 
+INSERT INTO sub_categories (category_id, sub_category) VALUES (12, 'Veterinary'); -- 55
+INSERT INTO sub_categories (category_id, sub_category) VALUES (12, 'Other'); -- 56
 
 --Other = 13
-INSERT INTO sub_categories (category_id, sub_category) VALUES (13, "Donations"); -- 57
-INSERT INTO sub_categories (category_id, sub_category) VALUES (13, "Other"); -- 58
+INSERT INTO sub_categories (category_id, sub_category) VALUES (13, 'Donations'); -- 57
+INSERT INTO sub_categories (category_id, sub_category) VALUES (13, 'Other'); -- 58
 
 -- income
 INSERT INTO income (user_id, income_date, amount) VALUES (1, '2023-03-13', 2000); 
@@ -139,8 +139,8 @@ INSERT INTO savings (saving_name, goal_amount, current_amount, finished, date_cr
 INSERT INTO savings (saving_name, goal_amount, current_amount, finished, date_created, date_finished, user_id) VALUES ('RRSP', 10000, 4000, TRUE, '2023-03-21', NULL, 1);
 
 -- debt_goals
-INSERT INTO debt_goals (name, initial_amount, amount_left, interest_rate, paid_off, user_id) VALUES ("Credit card", 2000, 2000, 15.00, FALSE, 1);
-INSERT INTO debt_goals (name, initial_amount, amount_left, interest_rate, paid_off, user_id) VALUES ("Student Loan", 20000, 20000, 5.00, FALSE, 1);
+INSERT INTO debt_goals (name, initial_amount, amount_left, interest_rate, paid_off, user_id) VALUES ('Credit card', 2000, 2000, 15.00, FALSE, 1);
+INSERT INTO debt_goals (name, initial_amount, amount_left, interest_rate, paid_off, user_id) VALUES ('Student Loan', 20000, 20000, 5.00, FALSE, 1);
 
 -- debt_payments
 INSERT INTO debt_payments (amount, debt_goal_id, user_id) VALUES (200, 1, 1);
