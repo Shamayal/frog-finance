a56eeba74a9d1e4d128c26eae5ec8296810abc34
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-06-12 11:16:19 -0400

    updated query semantics

--- backend/server/db/queries/getBudgetByMonth.js
+++ backend/server/db/queries/getBudgetByMonth.js
@@ -5,12 +5,12 @@ const db = require('../connection.js');
 const getBudgetByMonth = (userId, month, year) => {
   return db
     .query(`
-      select budgets.id, categories.category, budgets.budget_amount, budgets.total_spent, budgets.budget_reached 
-      from budgets 
-      JOIN categories ON budgets.category_id = categories.id  
-      where budgets.user_id = $1 
-      and EXTRACT(MONTH FROM budgets.updated_at) = $2  
-      and EXTRACT(YEAR FROM budgets.updated_at) = $3
+      SELECT budgets.id, categories.category, budgets.budget_amount, budgets.total_spent, budgets.budget_reached
+      from budgets
+      JOIN categories ON budgets.category_id = categories.id
+      WHERE budgets.user_id = $1
+      AND EXTRACT(MONTH FROM budgets.updated_at) = $2
+      AND EXTRACT(YEAR FROM budgets.updated_at) = $3
       ORDER BY budgets.total_spent`,[userId, month, year])
     .then((result) => {
       return result.rows;
