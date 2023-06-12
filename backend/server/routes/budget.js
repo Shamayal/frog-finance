4db175ffead6a9e94bb5154bb9f906a55e1eaeb4
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-06-12 08:26:21 -0400

    Updated the API with Month and Year

--- backend/server/routes/budget.js
+++ backend/server/routes/budget.js
@@ -2,17 +2,19 @@ const express = require('express');
 const router  = express.Router();
 const { getBudgetByMonth } = require('../db/queries/getBudgetByMonth');
 
-// GET/Budget/:month route to get the budget for that month
-router.get('/:budgetDate', (req, res) => {   //passing month and year as seperate aruguments, send the date will fetch month and year from the date
-  const budgetDate = req.params.budgetDate;
+// GET/Budget/:month/:year route to get the budget for that month
+router.get('/:month/:year', (req, res) => {   //passing month and year as seperate aruguments
+  const month = req.params.month;
+  const year = req.params.year;
+ 
   //const userId = req.session.userId;
-  const userId = 1;  //hardcoded the userId fro temporery purpose
-  getBudgetByMonth(userId, budgetDate)             
+  const userId = 1;                           //hardcoded the userId
+  getBudgetByMonth(userId, month, year)             
     .then((budget) => {
       console.log(budget);
-      const templateVars = {budget:budget, userId};
+      //const templateVars = {budget:budget, userId};
       //res.render('budget', templateVars);
-      res.send(budget);  //temporory added to populate in the browser
+      res.send(budget);                      //temporory added to populate in the browser
     })
     .catch((err) => {
       console.log(err);
