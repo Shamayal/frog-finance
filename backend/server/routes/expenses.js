e106f57568f4806a9510dbee04e613a654a3a40c
Shamayal Syed <shamayalsyed9@gmail.com>
2023-06-12 19:19:06 -0400

    query to get monthly expenses by category

--- backend/server/routes/expenses.js
+++ backend/server/routes/expenses.js
@@ -2,33 +2,27 @@ const income = require("./income");
 const router = require("express").Router();
 const db = require('../db/connection.js');
 
-
 // Total spent by category per month
   router.get("/:month/:year", (req, res) => {
 
-    const { month, year, userId } = req.params;
+    // const { month, year, userId } = req.params;
+
+    const month = 05;
+    const year = 2023;
 
     // query to display monthly expenses for the month selected by the user, split by categories and sub-categories
 
     // sum of all expenses per category
-    // for month, year, use
 
-    db.query(`SELECT user_id, categories.category AS category_name,
-    (SELECT SUM(amount)
+    db.query(`SELECT user_id, categories.category AS category_name, SUM(amount) AS total_amount
     FROM expenses
     JOIN users ON users.id = expenses.user_id
     JOIN sub_categories ON sub_categories.id = expenses.sub_category_id
     JOIN categories ON categories.id = sub_categories.category_id
-    WHERE users.id = expenses.user_id
-    GROUP BY categories.id
-    )
-    AS total_category_expense
-    FROM expenses
-    JOIN users ON users.id = expenses.user_id
-    WHERE EXTRACT(MONTH FROM expenses.expense_date) = 05
-    AND EXTRACT(YEAR FROM expenses.expense_date) = 2023
+    WHERE EXTRACT(MONTH FROM expenses.expense_date) = $1
+    AND EXTRACT(YEAR FROM expenses.expense_date) = $2
     AND users.id = expenses.user_id
-    GROUP BY category.category_name;`,[month, year])
+    GROUP BY categories.category, user_id;`,[month, year])
     .then((result) => {
       res.send({message: 'Total expenses by categories:', total_expenses_by_category: result.rows})
     })
@@ -40,7 +34,7 @@ const db = require('../db/connection.js');
 
   // Every expense transaction for a selected month
   router.get('/transactions', (req, res) => {
-    const month = 02;
+    const month = 05;
     const year = 2023;
 
     // const values = [`${year}-${month}-01`];
