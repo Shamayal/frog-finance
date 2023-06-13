9431a8ab3a736c254d1fcb988040110a0dad0b31
Shamayal Syed <shamayalsyed9@gmail.com>
2023-06-13 02:10:03 -0400

    refactored expenses get and post routes

--- backend/server/routes/expenses.js
+++ backend/server/routes/expenses.js
@@ -1,28 +1,17 @@
-const income = require("./income");
 const router = require("express").Router();
 const db = require('../db/connection.js');
+const { addExpense } = require("../db/queries/addExpense.js");
+const { getExpenseTransactions } = require("../db/queries/getExpenseTransactions.js");
+const { getExpenseByCategory } = require("../db/queries/getExpensesByCategory.js");
 
 // Total spent by category per month
   router.get("/:month/:year", (req, res) => {
 
-    // const { month, year, userId } = req.params;
+    const userId = 1; // const userId = req.session.userId;
+    const month = 05;   // const month = req.params.month;
+    const year = 2023;   // const year = req.params.year;
 
-    const month = 05;
-    const year = 2023;
-
-    // query to display monthly expenses for the month selected by the user, split by categories and sub-categories
-
-    // sum of all expenses per category
-
-    db.query(`SELECT user_id, categories.category AS category_name, SUM(amount) AS total_amount
-    FROM expenses
-    JOIN users ON users.id = expenses.user_id
-    JOIN sub_categories ON sub_categories.id = expenses.sub_category_id
-    JOIN categories ON categories.id = sub_categories.category_id
-    WHERE EXTRACT(MONTH FROM expenses.expense_date) = $1
-    AND EXTRACT(YEAR FROM expenses.expense_date) = $2
-    AND users.id = expenses.user_id
-    GROUP BY categories.category, user_id;`,[month, year])
+  getExpenseByCategory(userId, month, year)
     .then((result) => {
       res.send({message: 'Total expenses by categories:', total_expenses_by_category: result.rows})
     })
@@ -34,19 +23,11 @@ const db = require('../db/connection.js');
 
   // Every expense transaction for a selected month
   router.get('/transactions', (req, res) => {
-    const month = 05;
-    const year = 2023;
+    const userId = 1; // const userId = req.session.userId;
+    const month = 05;   // const month = req.params.month;
+    const year = 2023;   // const year = req.params.year;
 
-    db.query(`SELECT user_id,
-    to_char(expenses.expense_date, 'YYYY-MM-DD') AS expense_date,
-    amount, budget_id, categories.category AS category_name, sub_categories.sub_category AS sub_category_name
-    FROM expenses
-    JOIN users ON users.id = expenses.user_id
-    JOIN sub_categories ON sub_categories.id = expenses.sub_category_id
-    JOIN categories ON categories.id = sub_categories.category_id
-    WHERE EXTRACT(MONTH FROM expenses.expense_date) =$1
-    AND EXTRACT(YEAR FROM expenses.expense_date) = $2
-    AND users.id = expenses.user_id;`,[month, year])
+  getExpenseTransactions(userId, month, year)
     .then((result) => {
       res.send({message: 'All transactions for the selected month:', all_transactions_per_month: result.rows})
     })
@@ -59,10 +40,9 @@ const db = require('../db/connection.js');
   // the post req is for the budget tracker -> add expense route, users can add an expense they made
   router.post("/add", (req, res) => {
 
-    const query = `INSERT INTO expenses (user_id, expense_date, amount, sub_category_id) VALUES ($1, $2, $3, $4)`;
-    const values = [user_id, expense_date, amount, sub_category_id]
+    const { user_id, expense_date, amount, sub_category_id } = req.body;
 
-    db.query(query, values)
+  addExpense(user_id, expense_date, amount, sub_category_id)
     .then((result) => {
       res.send({message: 'Expense added successfully:', expense_added: result.rows})
     })
@@ -98,11 +78,3 @@ const db = require('../db/connection.js');
 // }
 
 module.exports = router;
-
-
-// db.query(`SELECT SUM(income.amount) AS total_income, SUM(expenses.amount) AS total_expenses, SUM(income.amount) - SUM(expenses.amount) AS net_total
-// FROM income
-// JOIN expenses ON expenses.user_id = income.user_id
-// WHERE EXTRACT(MONTH FROM income.income_date) = $1
-// AND EXTRACT(YEAR FROM income.income_date) = $2
-// AND income.user_id = $3;`,[month, year, user_id])
