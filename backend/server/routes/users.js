d32021c3316f820fcd552401ca9af4e03c3d4273
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-06-13 18:47:50 -0400

    notes and edits from meeting with Alvin - will use later to connect Auth0 to backend db

--- backend/server/routes/users.js
+++ backend/server/routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/logincheck', function(req, res, next) {
+  console.log("check req", req.body)
+
+  const authID = req.body.id;
+
+  // check if a user with this id exists
+  // if exist: select * from user where auth_id = id
+  // res.send
+
+  // else
+  // add auth_id to db
+  // res.send
+
+  res.send('respond with a resource');
+});
+
 module.exports = router;
