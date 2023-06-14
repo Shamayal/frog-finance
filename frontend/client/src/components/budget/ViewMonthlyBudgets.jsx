3f24b7f30447d69b289fd9cffc57490cb796c822
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-06-14 08:18:39 -0400

    Added the Budget and Expense Graphs

--- frontend/client/src/components/budget/ViewMonthlyBudgets.jsx
+++ frontend/client/src/components/budget/ViewMonthlyBudgets.jsx
@@ -65,7 +65,36 @@ const ViewMonthlyBudgets = () => {
             ))}
           </tbody>
         </table></>
-        <div>
+        <table><tbody><tr><td>
+          <h2 style={{ textAlign: "center" }}>Budget Pie Chart</h2>
+          <Pie
+            data={{
+              labels: monthlyBudget.map((data) => data.category),
+              datasets: [{
+                //label: 'Expenses on Category',
+                data: monthlyBudget.map((data) => data.budget_amount.toLocaleString()),
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(255, 205, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: ['rgb(255, 99, 132)',
+                  'rgb(255, 159, 64)',
+                  'rgb(255, 205, 86)',
+                  'rgb(75, 192, 192)',
+                  'rgb(54, 162, 235)',
+                  'rgb(153, 102, 255)',
+                  'rgb(201, 203, 207)'
+                ],
+                borderWidth: 1
+              }]
+            }} />
+        </td>
+        <td>
           <h2 style={{ textAlign: "center" }}>Expenses Pie Chart</h2>
           <Pie
             data={{
@@ -93,9 +122,39 @@ const ViewMonthlyBudgets = () => {
                 borderWidth: 1
               }]
             }} />
-        </div>
-        <div>
+        </td>
+        </tr>
+        <tr><td>
           <h2 style={{ textAlign: "center" }}>Expenses Bar Chart</h2>
+          <Bar
+            data={{
+              labels: monthlyBudget.map((data) => data.category),
+              datasets: [{
+                label: 'Budget on Category',
+                data: monthlyBudget.map((data) => data.budget_amount.toLocaleString()),
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(255, 205, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: ['rgb(255, 99, 132)',
+                  'rgb(255, 159, 64)',
+                  'rgb(255, 205, 86)',
+                  'rgb(75, 192, 192)',
+                  'rgb(54, 162, 235)',
+                  'rgb(153, 102, 255)',
+                  'rgb(201, 203, 207)'
+                ],
+                borderWidth: 1
+              }]
+            }} />
+            </td>
+            <td>
+            <h2 style={{ textAlign: "center" }}>Expenses Bar Chart</h2>
           <Bar
             data={{
               labels: monthlyBudget.map((data) => data.category),
@@ -122,7 +181,8 @@ const ViewMonthlyBudgets = () => {
                 borderWidth: 1
               }]
             }} />
-        </div></>
+            </td></tr></tbody>
+        </table></>
       }
     </div>
   )
