f3dbd4b73f9fbf1b61d7736d509ce7a7b6e02288
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-06-15 11:14:06 -0400

    update savings component, route, and seeds to change 'finished = false' to finished = 'true' when a savings goal is reached

--- backend/server/db/queries/getSavingsQueries.js
+++ backend/server/db/queries/getSavingsQueries.js
@@ -1,11 +1,38 @@
 const db = require('../connection.js');
 
+const getCurrentSavingsGoalAfterUpdate = (user_id) => {
+  return updatedCompletedSavingsGoals(user_id)
+  .then(() => getCurrentSavingsGoal(user_id))
+}
+
+const updatedCompletedSavingsGoals = (user_id) => {
+  return db.query(
+    `
+    UPDATE savings SET finished = TRUE, date_finished = CURRENT_DATE
+    WHERE finished = FALSE
+    AND goal_amount <= (
+      SELECT SUM(total) FROM (
+        SELECT SUM(amount) AS total FROM income WHERE user_id = 1
+        UNION
+        SELECT SUM(amount) * -1 AS total  FROM debt_payments WHERE user_id = 1
+        UNION
+        SELECT SUM(amount) * -1 AS total  FROM expenses WHERE user_id = 1
+        UNION
+        SELECT SUM(goal_amount) * -1 AS total FROM savings WHERE user_id = 1 AND finished = true
+        ) AS totals
+    )
+    AND savings.user_id = $1;
+    `, [user_id]
+  )
+}
+
 const getCurrentSavingsGoal = (user_id) => {
   return db.query(`
   SELECT users.id AS user_id, savings.id AS savings_id, savings.saving_name, savings.goal_amount,
     (SELECT SUM(amount) FROM income WHERE user_id = savings.user_id) -
     ((SELECT SUM(amount) FROM debt_payments WHERE user_id = savings.user_id) +
-    (SELECT SUM(amount) FROM expenses WHERE user_id = savings.user_id))
+    (SELECT SUM(amount) FROM expenses WHERE user_id = savings.user_id) +
+    (SELECT SUM(goal_amount) FROM savings WHERE user_id = savings.user_id AND finished = TRUE))
   AS current_amount,
   to_char(date_created, 'YYYY-MM-DD') AS date_created,
   finished
@@ -15,12 +42,15 @@ const getCurrentSavingsGoal = (user_id) => {
   AND users.id = $1;
   `, [user_id])
     .then((result) => {
-      return result;
+      const goal = result.rows[0];
+        return goal;
     })
     .catch((err) => {
       console.log(err.message);
     });
 }
+
+
 
 const getFinishedSavingsGoal = (user_id) => {
   return db.query(`
@@ -50,7 +80,7 @@ const createNewSavingsGoal = (savings_name, goal_amount, current_amount, user_id
 }
 
 module.exports = {
-  getCurrentSavingsGoal,
   getFinishedSavingsGoal,
-  createNewSavingsGoal
+  createNewSavingsGoal,
+  getCurrentSavingsGoalAfterUpdate
 }
--- frontend/client/src/components/savings/CreateSavingsGoal.jsx
+++ frontend/client/src/components/savings/CreateSavingsGoal.jsx
@@ -2,14 +2,17 @@ import React, { useState, useContext, useEffect } from 'react';
 import { useSavingsHook } from '../../hooks/savings';
 
 
+
+
 const CreateSavingsGoal = () => {
-  const [ savingName, setSavingName ] = useState("")
-  const [ goalAmount, setGoalAmount ] = useState("")
-  const [ currentAmount, setCurrentAmount ] = useState("")
+  const [savingName, setSavingName] = useState("")
+  const [goalAmount, setGoalAmount] = useState("")
+  const [currentAmount, setCurrentAmount] = useState("")
 
 
-    const { createSavingsGoal } = useSavingsHook();
+  const { viewSavingsGoal, createSavingsGoal, currentSavingsGoal  } = useSavingsHook();
 
+  console.log("creat goal sg state", currentSavingsGoal)
 
   const handleClick = (event) => {
     event.preventDefault()
@@ -19,52 +22,60 @@ const CreateSavingsGoal = () => {
   return (
     <div>
       <h1>Create New Savings Goal: </h1>
-      <form action="">
 
-        <label htmlFor="saving_name" className="sr-only">
-          What are you saving for? Enter your goal name:
-        </label>
-        <input
-          type="text"
-          value={savingName}
-          name="saving_name"
-          id="saving_name"
-          placeholder='goal name'
-          onChange={(event) => setSavingName(event.target.value)}
-        />
+      {currentSavingsGoal !== null && <p>You already have an active savings goal. Click here to view it</p>}
 
-        <br />
+      {currentSavingsGoal === null &&
+
+        <form action="">
+
+          <label htmlFor="saving_name" className="sr-only">
+            What are you saving for? Enter your goal name:
+          </label>
+          <input
+            type="text"
+            value={savingName}
+            name="saving_name"
+            id="saving_name"
+            placeholder='goal name'
+            onChange={(event) => setSavingName(event.target.value)}
+          />
+
+          <br />
 
 
-        <label htmlFor="goal_amount" className="sr-only">
-          How much do you need to save to reach this goal?
-        </label>
-        <input
-          type="number" value={goalAmount}
-          name="goal_amount"
-          id="goal_amount"
-          placeholder='$0'
-          onChange={(event) => setGoalAmount(event.target.value)}
-        />
+          <label htmlFor="goal_amount" className="sr-only">
+            How much do you need to save to reach this goal?
+          </label>
+          <input
+            type="number" value={goalAmount}
+            name="goal_amount"
+            id="goal_amount"
+            placeholder='$0'
+            onChange={(event) => setGoalAmount(event.target.value)}
+          />
 
-        <br />
+          <br />
 
-        <label htmlFor="current_amount" className="sr-only">
-          How much have you already saved towards this goal?
-        </label>
-        <input
-          type="number"
-          value={currentAmount}
-          name="current_amount"
-          id="current_amount"
-          placeholder='$0'
-          onChange={(event) => setCurrentAmount(event.target.value)}
-        />
+          <label htmlFor="current_amount" className="sr-only">
+            How much have you already saved towards this goal?
+          </label>
+          <input
+            type="number"
+            value={currentAmount}
+            name="current_amount"
+            id="current_amount"
+            placeholder='$0'
+            onChange={(event) => setCurrentAmount(event.target.value)}
+          />
 
-        <br />
+          <br />
 
-        <button type="submit" onClick={handleClick}>Create Goal</button>
-      </form>
+          <button type="submit" onClick={handleClick}>Create Goal</button>
+        </form>
+
+      }
+
 
 
     </div>
