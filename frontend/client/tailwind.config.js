6ffd205cba3a44cda646740ce087211a4bff5b2c
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-06-16 10:09:48 -0400

    install and configure tailwind

---
+++ frontend/client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: 'white',
+        none: 'none'
+      },
+      borderWidth: {
+        1: '1px',
+      },
+      fontFamily: {
+        quicksand: ['Quicksand', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
