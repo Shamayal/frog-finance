c2fbf1f1342be42458f3bd96ad4b5ba00ceca800
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-06-15 20:38:18 -0400

    set up debt pages/route with corresponding component pieces

--- frontend/client/src/routes/Debt/DebtProgress.jsx
+++ frontend/client/src/routes/Debt/DebtProgress.jsx
@@ -1,8 +1,12 @@
+import CurrentDebtGoals from '../../components/debt/CurrentDebtGoals'
+
 const DebtProgress = () => {
   return (
     <div>
       <h2>This is the Debt Progress Page</h2>
       <h4>Users can add a see their debt in a graph and how much they have left to pay off</h4>
+
+      <CurrentDebtGoals />
     </div>
   );
 };
