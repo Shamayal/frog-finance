87ae301c8fb3fb915f55f712070a7f68be442e89
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-06-16 08:09:40 -0400

    add progress bars with % complete

--- frontend/client/src/components/debt/CurrentDebtGoals.jsx
+++ frontend/client/src/components/debt/CurrentDebtGoals.jsx
@@ -4,39 +4,45 @@ import { useDebtHook } from '../../hooks/debt';
 const ViewDebtGoals = () => {
   const { viewDebtGoals, currentDebtGoals } = useDebtHook();
 
-  console.log("check fe cdg", currentDebtGoals)
-
   useEffect(() => {
     viewDebtGoals();
   }, []);
+
+
 
   return (
     <div>
       <h1>Current Debt Goals:</h1>
 
-      {currentDebtGoals.length === 0 && <p>No debt goals yet</p>
-      }
+      {currentDebtGoals.length === 0 && <p>No debt goals yet</p>}
 
       {currentDebtGoals.length > 0 && (
-        currentDebtGoals.map((debtGoal) => (
-          <section key={debtGoal.debt_id}>
-            <h3>Goal Name: {debtGoal.name}</h3>
-            <p>Initial amount: ${debtGoal.initial_amount}</p>
-            <p>Interest Rate: {debtGoal.interest_rate}%</p>
-            {debtGoal.amount_paid === null && <p>Amount Paid Off: $0</p>}
-            {debtGoal.amount_paid !== null && <p>Amount Paid Off: ${debtGoal.amount_paid}</p>}
-            <p>Amount Left: ${debtGoal.amount_left}</p>
-            {/* <p>Amount Left: ${!debtGoal.amount_left && "mark complete"}</p> */}
-            <p>Debt id: {debtGoal.debt_id}</p>
-            <button>
-              Update payments
-            </button>
-          </section>
-        ))
-      )}
+        currentDebtGoals.map((debtGoal) => {
+          const width = (debtGoal.amount_paid / debtGoal.initial_amount) * 100;
 
+          return (
+            <section key={debtGoal.debt_id}>
+              <h3>Goal Name: {debtGoal.name}</h3>
+              <p>Initial amount: ${debtGoal.initial_amount}</p>
+              <p>Interest Rate: {debtGoal.interest_rate}%</p>
+              {debtGoal.amount_paid === null && <p>Amount Paid Off: $0</p>}
+              {debtGoal.amount_paid !== null && <p>Amount Paid Off: ${debtGoal.amount_paid}</p>}
+              <p>Amount Left: ${debtGoal.amount_left}</p>
+              <p>Debt id: {debtGoal.debt_id}</p>
+              <p>Progress:</p>
+              <div className="progress w-50" role="progressbar" aria-label="Success example" aria-valuenow={width} aria-valuemin={0} aria-valuemax={100}>
+                <div className="progress-bar bg-success" style={{ width: `${width}%` }}>{width}%</div>
+              </div>
+              <button className='btn btn-light'>
+                Update payments
+              </button>
+            </section>
+          );
+        })
+      )}
     </div>
   );
+
 };
 
 export default ViewDebtGoals;
--- frontend/client/src/components/savings/CurrentSavingsGoal.jsx
+++ frontend/client/src/components/savings/CurrentSavingsGoal.jsx
@@ -7,9 +7,15 @@ const ViewSavingsGoals = () => {
   const { viewSavingsGoal, currentSavingsGoal } = useSavingsHook();
   console.log("current savings goal state", currentSavingsGoal)
 
-useEffect(() => {
-  viewSavingsGoal()
-}, [])
+  useEffect(() => {
+    viewSavingsGoal()
+  }, [])
+
+  const currentAmount = parseInt(currentSavingsGoal.current_amount);
+
+  const width = (currentSavingsGoal.goal_amount / currentAmount).toFixed(0);
+
+  console.log('check width', width)
 
   return (
     <div>
@@ -18,12 +24,21 @@ useEffect(() => {
       {!currentSavingsGoal && <p>You dont have any savings goals yet</p>}
 
       {currentSavingsGoal &&
-      <>
-        <p>Goal Name: {currentSavingsGoal.saving_name}</p>
-        <p>Goal Amount: ${currentSavingsGoal.goal_amount}.00</p>
-        <p>Currently Saved: ${currentSavingsGoal.current_amount}.00</p>
-        <p>Date Created: {currentSavingsGoal.date_created}</p>
-      </>
+        <>
+          <p>Goal Name: {currentSavingsGoal.saving_name}</p>
+          <p>Goal Amount: ${currentSavingsGoal.goal_amount}.00</p>
+          <p>Currently Saved: ${currentSavingsGoal.current_amount}.00</p>
+          <p>Date Created: {currentSavingsGoal.date_created}</p>
+
+          <p>Progress:</p>
+
+          <div className="progress w-50" role="progressbar" aria-label="Success example" aria-valuenow={25} aria-valuemin={0} aria-valuemax={100}>
+            <div className="progress-bar bg-success" style={{ width: `${width}%` }}>{width}%</div>
+
+          </div>
+
+
+        </>
       }
     </div>
   )
