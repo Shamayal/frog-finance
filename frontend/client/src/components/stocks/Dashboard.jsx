0c4399aa03d8d5459699f05c958daa1abd3a3232
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-06-16 12:48:25 -0400

    search bar component and search icons

--- frontend/client/src/components/stocks/Dashboard.jsx
+++ frontend/client/src/components/stocks/Dashboard.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Card from "./Card";
+import { mockCompanyDetails } from "../../constants/mock";
+import Search from "./Search";
 
 const Dashboard = ({children}) => {
   return (
     <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand">
+
       <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1">
-        <Card> Header </Card>
+        <h1 className="text-5xl"> {mockCompanyDetails.name}</h1>
+        <Search />
       </div>
+
       <div className="md:col-span-2 row-span-4">
       <Card> Chart </Card>
       </div>
