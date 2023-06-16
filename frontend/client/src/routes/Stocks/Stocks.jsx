453b5760c613b2a8ee918e7c6e38d9df0fd99351
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-06-16 11:05:54 -0400

    initial stocks page component and grid layout

--- frontend/client/src/routes/Stocks/Stocks.jsx
+++ frontend/client/src/routes/Stocks/Stocks.jsx
@@ -1,9 +1,8 @@
+import Dashboard from "../../components/stocks/Dashboard";
+
 const Stocks = () => {
   return (
-    <div>
-      <h2>This is the Stocks Page</h2>
-      <h4>Here we will use an API to display stocks!</h4>
-    </div>
+    <Dashboard />
   );
 };
 
--- frontend/client/tailwind.config.js
+++ frontend/client/tailwind.config.js
@@ -1,22 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        white: 'white',
-        none: 'none'
-      },
-      borderWidth: {
-        1: '1px',
+        white: "white",
+        none: "none",
       },
       fontFamily: {
-        quicksand: ['Quicksand', 'sans-serif']
-      }
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
+      },
     },
   },
   plugins: [],
-}
-
+};
