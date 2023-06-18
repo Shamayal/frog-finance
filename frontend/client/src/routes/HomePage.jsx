756f646f506530928b7c4e5e96e0c0ea9ba3dc97
Shamayal Syed <shamayalsyed9@gmail.com>
2023-06-17 20:39:28 -0400

    added video and logo

--- frontend/client/src/routes/HomePage.jsx
+++ frontend/client/src/routes/HomePage.jsx
@@ -1,19 +1,16 @@
 import "../../src/styles/HomePage.css"
-import coin_spinning from "../media/coin_spinning.mp4";
+import frog from "../media/frog.mp4"
+import frogLeaf from "../media/frog_leaf.mp4"
 
 const HomePage = () => {
   return (
-    <div>
-      <h2>Welcome to Frog Finance!</h2>
-      <h3>Leap towards financial stability!</h3>
-      <h4>Join millions of others and hop into finance frog to embark on your journey to effective money management!</h4>
-      <h3>The First Hop Towards a Better Future!</h3>
-      <h6>Disclaimer: No frogs were harmed in the making of this blog post. Ribbit!</h6>
-      <div className="homepage">
-        <video autoPlay loop muted playsInline className="back-video">
-          <source src={coin_spinning} type="video/mp4">
-          </source>        
-        </video>
+    <div className="homepage">
+      <video autoPlay loop muted playsInline className="back-video">
+        <source src={frogLeaf} type="video/mp4" />        
+      </video>
+      <div className="homepage-content">
+        <h1>Frog Finance</h1>
+        <h3>Leap towards financial stability!</h3>
       </div>
     </div>
   );
