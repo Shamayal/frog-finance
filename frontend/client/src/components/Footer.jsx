d7ddb8452cb30b31a1664e5608f3b75b5ad27103
Shamayal Syed <shamayalsyed9@gmail.com>
2023-06-19 04:52:26 -0400

    footer removed social media icons and replaced with logo

--- frontend/client/src/components/Footer.jsx
+++ frontend/client/src/components/Footer.jsx
@@ -14,17 +14,10 @@ const Footer = () => {
           </ul>
         </div>
         <div className="social-icons">
-          <i className="fa-brands fa-facebook-f"></i>
-          <i className="fa-brands fa-twitter"></i>
-          <i className="fa-brands fa-instagram"></i>
-          <i className="fa-brands fa-youtube"></i>
-          <FontAwesomeIcon icon="sack-dollar" className="modalIcon" />
-          <i className="fa-brands fa-facebook fa-spin fa-spin-reverse" style={{ color: '#526e11' }}></i>
-
+          <FontAwesomeIcon icon="frog" />
         </div>
         <div className="footer-title fontweight-700 font-poppins">
           <h2>Frog Finance</h2>
-          <h5 className="font-quicksand"><em>The first hop towards financial freedom!</em></h5>
         </div>
         <div className="footer-copyright font-quicksand">
           <h6>© 2023: Frog Finance, Inc. All rights reserved.</h6>
