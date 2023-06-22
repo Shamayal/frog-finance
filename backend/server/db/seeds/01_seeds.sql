619cf2a44266be17bb284f15d16cd95f222407ad
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-06-22 10:37:00 -0400

    remove RRSP from completed saving goals

--- backend/server/db/seeds/01_seeds.sql
+++ backend/server/db/seeds/01_seeds.sql
@@ -156,7 +156,7 @@ INSERT INTO expenses (user_id, expense_date, amount, category_id, sub_category_i
 INSERT INTO expenses (user_id, expense_date, amount, category_id, sub_category_id) VALUES (1, '2023-05-16', 100, 12, 52); -- pets, Food
 
 -- savings
-INSERT INTO savings (saving_name, goal_amount, current_amount, finished, date_created, date_finished, user_id) VALUES ('RRSP', 1000, 1000, TRUE, '2023-01-21', '2023-06-02', 1);
+-- INSERT INTO savings (saving_name, goal_amount, current_amount, finished, date_created, date_finished, user_id) VALUES ('RRSP', 1000, 1000, TRUE, '2023-01-21', '2023-06-02', 1);
 INSERT INTO savings (saving_name, goal_amount, current_amount, finished, date_created, date_finished, user_id) VALUES ('Desk', 100, 100, TRUE, '2023-05-21', '2023-05-30', 1);
 
 -- debt_goals
